8c6e783c54c23b477abc8c397cb6f2e172625192
myavkat <mehmetyavuzkatranci@gmail.com>
2021-02-16 03:35:30 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(10,20))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            
+
+            
+        }
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Entities.Abstract;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class : referans tip
+    //IEntity : IEntity veya IEntity'yi implemente eden nesnelerden olabilir
+    //new() : newlenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
